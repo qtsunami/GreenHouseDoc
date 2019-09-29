3e4b6dce6b8f89e0c0dd7d004887894f61026f48
Greggwen <simple.xull@gmail.com>
2019-09-29 16:45:20 +0800

    adjust entries file

--- ghdview/src/components/Header.vue
+++ ghdview/src/components/Header.vue
@@ -2,11 +2,11 @@
     <el-container>
         <h1 style="color: #da5fb1; font-size: 20px; margin-right: 100px;">GreenHouseDoc</h1>
         <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-            <el-menu-item index="1"><router-link to="/">首页</router-link></el-menu-item>
-            <el-menu-item index="2"><router-link to="/project">项目</router-link></el-menu-item>
-            <el-menu-item index="3"><router-link to="/project">文章</router-link></el-menu-item>
-            <el-menu-item index="4"><router-link to="/project">读书笔记</router-link></el-menu-item>
-            <el-menu-item index="5"><router-link to="/project">我的工作台</router-link></el-menu-item>
+            <el-menu-item index="1"><a href="/index.html">首页</a></el-menu-item>
+            <el-menu-item index="2"><a href="/project.html">项目</a></el-menu-item>
+            <el-menu-item index="3"><a href="/article.html">文章</a></el-menu-item>
+            <el-menu-item index="4"><a href="/booknote.html">读书笔记</a></el-menu-item>
+            <el-menu-item index="5"><a href="/workbench.html">我的工作台</a></el-menu-item>
         </el-menu>
         <span style="position: absolute; font-size: 14px; right: 50px;">欢迎您，<b>王小虎</b></span>
     </el-container>
@@ -19,10 +19,10 @@
 <script>
     export default {
         data() {
-        return {
-            activeIndex: '1',
-            activeIndex2: '1'
-        };
+            return {
+                activeIndex: '1',
+                activeIndex2: '1'
+            };
         },
         methods: {
             handleSelect(key, keyPath) {
